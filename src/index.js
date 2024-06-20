c72e9b8e6816530901f11e98752e9138cfc1bd94
Tuupi <fajiom@gmail.com>
2024-06-20 18:35:36 +0700

    adding tailwind

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { initializeApp } from 'firebase/app'
+import { 
+    getFirestore, collection, getDocs,
+    addDoc,
+    Timestamp
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDFnkeNCEUse2BLRAcKvJuKyjpmFicLcdQ",
@@ -11,3 +16,38 @@ const firebaseConfig = {
 };
 
 initializeApp(firebaseConfig)
+
+const db = getFirestore()
+
+const colRef = collection(db, 'comments')
+
+getDocs(colRef).then((snapshot) => {
+    let comments = []
+    snapshot.docs.forEach((doc) => {
+        comments.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(comments)
+    comments.forEach((comment) => {
+        var div = document.createElement(div);
+        div.classList.add("");
+        div.innerHTML = comment.name + "<br>" + comment.email + "<br>" + comment.comment + "<br>";
+        document.getElementById("commentcontent").appendChild(div);
+    })
+})
+
+
+const addCommentForm = document.querySelector('.Comment-form')
+addCommentForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        name: addCommentForm.name.value,
+        email: addCommentForm.email.value,
+        comment: addCommentForm.comment.value,
+        date: Timestamp.now()
+    }).then(() => {
+        addCommentForm.reset()
+    })
+
+})
+    
